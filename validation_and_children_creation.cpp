eaa1d4f3873cdf2a4d67bf3cefefe1b5bb225373
FaresSalem <faressalem98@gmail.com>
2021-06-08 18:01:46 +0200

    Update validation_and_children_creation.cpp

--- validation_and_children_creation.cpp
+++ validation_and_children_creation.cpp
@@ -1,21 +1,6 @@
-#include <iostream>
 #include <stdint.h>
 
 enum type {HUMAN, CPU};
-enum mode {STEALING, NO_STEALING};
-
-struct state_info
-{
-	int8_t state[14];
-	bool another_turn;
-	bool game_ended;
-	int score;
-};
-
-struct arrays_collection
-{
-	state_info children[6];
-};
 
 bool is_valid(int8_t* state, type player_type, uint8_t player_move)
 {
@@ -27,30 +12,26 @@ bool is_valid(int8_t* state, type player_type, uint8_t player_move)
 		return true;
 }
 
-arrays_collection get_children(int8_t* state, mode playing_mode, type player_type)
+void Get_next_state_indices(int8_t state[], int8_t indices[], type player_type)
 {
-	arrays_collection result;
-	int8_t* obtained_child;
-	
-	// tracking state's original value, it will change at calling get_next_state()
-	uint8_t old_state[14];			
-
-	for(uint8_t u = 0; u < 14; u++)
-		old_state[u] = state[u];
-
-	for (uint8_t k = 0; k < 6; k++)
+	if (player_type == CPU)
 	{
-		if (is_valid(state, player_type, k + 1))
+		for (int i = 0; i < 6; i++)
 		{
-			//obtained_child = get_next_state(state, playing_mode, player_type, k + 1).state;
-			for(uint8_t h = 0; h < 14; h++)
-				(((result.children)[k]).state)[h] = obtained_child[h];
+			if (state[i] == 0)
+				indices[i] = -1;
+			else
+				indices[i] = i + 1;	// make indices between 1 and 6
 		}
-		else	//invalid child due to invalid move
-			(((result.children)[k]).state)[0] = -1;
-			
-		for(uint8_t u = 0; u < 14; u++)
-			state[u] = old_state[u];	// restoring the original value of the state
 	}
-	return result;
+	else
+	{
+		for (int i = 7; i < 13; i++)
+		{
+			if (state[i] == 0)
+				indices[i] = -1;
+			else
+				indices[i] = i - 6 ;	// make indices between 1 and 6
+		}
+	}
 }
